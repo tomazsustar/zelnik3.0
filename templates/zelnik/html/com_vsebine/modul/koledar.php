2c6fc018be42ed923d374b219777f8dc0a5bd5f1
Tomaž Šuštar <tomaz@zelnik.net>
2013-07-29 23:36:30 +0200

    a je kej za committat?

--- templates/zelnik/html/com_vsebine/modul/koledar.php
+++ templates/zelnik/html/com_vsebine/modul/koledar.php
@@ -30,7 +30,7 @@ JHtml::_('behavior.caption');
 <table>
   
 		<?php 
-			echo __FILE__;
+			//echo __FILE__;
 			foreach ($this->items as $datum => $dogodki) : 
 				list($leto, $mesec, $dan) = explode('-', $datum);
 				?>
