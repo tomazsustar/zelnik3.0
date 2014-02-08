c8bf44acb5649e54d0a91fb66fa8438e3fdf37cf
unknown <poje.tilen@gmail.com>
2014-02-08 14:41:52 +0100

    Update

--- components/com_vsebine/router.php
+++ components/com_vsebine/router.php
@@ -15,19 +15,32 @@ defined('_JEXEC') or die;
  * @return	array
  */
  
-function GetTitle($PrispevekId) {
+function GetTagId($Tag) {
 	$db = JFactory::getDbo();
 	$query = $db->getQuery(true);
 	
-	$query->from('nize01_zelnik.vs_vsebine AS v');
-	$query->select('v.title');
-	$query->where('v.id = "'.$PrispevekId.'"');
+	$query->from('nize01_zelnik.vs_tags AS t');
+	$query->select('t.id');
+	$query->where('t.tag = "'.$Tag.'"');
 	
 	$db->setQuery($query);
 	$Row = $db->loadObject();
-	$Title = JFilterOutput::stringURLSafe($Row->title);
 	
-    return $Title;
+    return $Row->id;
+}
+
+function ParseOutTag($TagId) {
+	$db = JFactory::getDbo();
+	$query = $db->getQuery(true);
+	
+	$query->from('nize01_zelnik.vs_tags AS t');
+	$query->select('t.tag');
+	$query->where('t.id = "'.$TagId.'"');
+	
+	$db->setQuery($query);
+	$Row = $db->loadObject();
+
+    return $Row->tag;
 }
 
 function VsebineBuildRoute(&$query)
@@ -35,21 +48,22 @@ function VsebineBuildRoute(&$query)
 	$segments = array();
 	
 	if(isset($query['prispevek'])) {
+		$segments[] = 'Prispevek';
 		$segments[] = $query['prispevek'];
-		unset($query['prispevek']);
 		
 		if(isset($query['title'])) {
 			$segments[] = $query['title'];
 			unset($query['title']);
 		}
+		unset($query['prispevek']);
 	}
-	else if(isset($query['tag'])) {
-		$segments[] = $query['tag'];
-		unset($query['tag']);
-	}
-	else if(isset($query['tags'])) {
-		$segments[] = $query['tags'];
-		unset($query['tags']);
+	else if(isset($query['tag']) || isset($query['tags'])) {
+		$segments[] = 'Tag';
+		$segments[] = (isset($query['tag']) ? GetTagId($query['tag']) : GetTagId($query['tags']));
+		$segments[] = (isset($query['tag']) ? JFilterOutput::stringURLSafe($query['tag']) : JFilterOutput::stringURLSafe($query['tags']));
+		
+		if(isset($query['tag'])) unset($query['tag']);
+		else unset($query['tags']);
 	}
 
 	return $segments;
@@ -59,14 +73,15 @@ function VsebineParseRoute($segments)
 {
 	$vars = array();
 	
-	if(is_numeric($segments[0])) {
-		$vars['prispevek'] = $segments[0];
-		if(isset($segments[1]))
-			$vars['title'] = $segments[1];
+	if($segments[0] == 'Prispevek') {
+		$vars['prispevek'] = $segments[1];
+		if(isset($segments[2]))
+			$vars['title'] = $segments[2];
 		$vars['view'] = 'prispevek';
 	}
-	else {
-		$vars['tags'] = $segments[0];
+	else if($segments[0] == 'Tag'){
+		$var['tag'] = ParseOutTag($segments[1]);
+		$vars['tags'] = ParseOutTag($segments[1]);
 		$vars['view'] = 'vsebine';
 	}
 	
