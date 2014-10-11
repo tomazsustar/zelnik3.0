c8c44b74a1049c92430b6abceddc0dbffde30440
Tomaž Šuštar <tomaz@zelnik.net>
2014-10-12 01:50:27 +0200

    naslovka

--- components/com_vsebine/models/vsebine.php
+++ components/com_vsebine/models/vsebine.php
@@ -117,8 +117,8 @@ class VsebineModelVsebine extends JModelList {
         		$query->where("t.name IN ($tags)");
         	}else{
         		$query->where("(a.publish_down > current_timestamp or a.publish_down is null or a.publish_down='0000-00-00') and a.publish_up <= current_timestamp");
-        		$query->join('LEFT', '`nize01_cinovicomat`.vs_tags_content as tc ON tc.content_id = c.id');
-        		$query->where('(a.frontpage = 1 OR tc.tag_id=815)');
+        		$query->join('LEFT', '`nize01_cinovicomat`.vs_media_content as mc ON mc.content_id = c.id and mc.type=menu');
+        		$query->where('(a.frontpage = 1 OR mc.media_id=4)');
         	}
         	 
         	$query=str_replace("SELECT", "SELECT DISTINCT", $query);
