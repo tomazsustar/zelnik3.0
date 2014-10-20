53647c7d9ea1d2576589fa9be92d894833a7a010
Tomaž Šuštar <tomaz@zelnik.net>
2014-10-20 20:07:43 +0200

    tags - ugly fix

--- components/com_vsebine/router.php
+++ components/com_vsebine/router.php
@@ -23,7 +23,7 @@ function GetTag($Tag) {
 	if($version){
 		$query->from('nize01_cinovicomat.vs_tags AS t');
 		$query->select('t.alias, t.name as tag');
-		$query->where("t.alias LIKE '$Tag' OR t.name LIKE '".str_replace("-", " ", $Tag)."'");
+		 $query->where("t.alias LIKE '$Tag' OR t.name LIKE '".str_replace("-", " ", $Tag)."'");
 	}else{
 		$query->from('nize01_zelnik.vs_tags AS t');
 		$query->select('t.*');
@@ -31,8 +31,6 @@ function GetTag($Tag) {
 	}
 	$db->setQuery($query);
 	$Row = $db->loadObject();
-	if(!isset($Row))
-		echo("".$query);
     return $Row;
 }
 
